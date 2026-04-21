cc86ad9b22d8a8bfa62a04b52d6f774ba21b151a
Yashini Pardeshi <yashini.pardeshi@coditas.com>
2026-04-21 21:30:04 +0530

    Update main.tf - oidc removal

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 terraform{
- backend "azurerm" {
+   backend "azurerm" {
     storage_account_name = "storeaccghgy"
     container_name       = "terraform-state-dev"
     key                  = "jenkins-terraform.tfstate"
-    use_azuread_auth     = true
-    use_oidc             = true
     subscription_id      = "9919455d-a384-407d-849e-190bad4af9d7"
     tenant_id            = "550d7d5c-247a-472e-af33-9915c948872b"
     resource_group_name  = "azure-terraform-git"
